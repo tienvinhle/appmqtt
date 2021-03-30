ad4ef83b981ee90fe02438995eea611dc66ab6b9
tienvinhle <tienvinh.industrial.automation@gmail.com>
2021-03-30 22:37:12 +0700

    edit async

--- hbmqttClient.py
+++ hbmqttClient.py
@@ -8,28 +8,26 @@ from hbmqtt.mqtt.constants import QOS_1, QOS_2
 # This sample shows how to publish messages to broker using different QOS
 # Debug outputs shows the message flows
 #
-@asyncio.coroutine
-def test_coro():
-    C = MQTTClient()
-    yield from C.connect('mqtt://113.161.79.146:5000/')
-    tasks = [
-        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_0')),
-        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_1', qos=QOS_1)),
-        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_2', qos=QOS_2)),
-    ]
-    yield from asyncio.wait(tasks)
-    yield from C.disconnect()
+#@asyncio.coroutine
+#def test_coro():
+#    C = MQTTClient()
+#    yield from C.connect('mqtt://113.161.79.146:5000/')
+#    tasks = [
+#        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_0')),
+#        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_1', qos=QOS_1)),
+#        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_2', qos=QOS_2)),
+#    ]
+#    yield from asyncio.wait(tasks)
+#    yield from C.disconnect()
 
-
-@asyncio.coroutine
-def test_coro2():
+async def test_coro2():
     try:
         C = MQTTClient()
-        yield from C.connect('mqtt://iot2021:iot2021@113.161.79.146:5000/')
-        yield from C.publish('$SYS/iot2050No1/temp', b'30', qos=0x00)
-        yield from C.publish('$SYS/iot2050No1/humidity', b'80', qos=0x01)
-        yield from C.publish('$SYS/iot2050No1/pressure', b'5', qos=0x02)
-        yield from C.disconnect()
+        await C.connect('mqtt://iot2021:iot2021@113.161.79.146:5000/')
+        await C.publish('$SYS/iot2050No1/temp', b'30', qos=0x00)
+        await C.publish('$SYS/iot2050No1/humidity', b'80', qos=0x01)
+        await C.publish('$SYS/iot2050No1/pressure', b'5', qos=0x02)
+        await C.disconnect()
     except ConnectException as ce:
         asyncio.get_event_loop().stop()
 
