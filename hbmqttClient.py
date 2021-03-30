9f8620c881a61be924dcc9dab01a624e2823f482
tienvinhle <tienvinh.industrial.automation@gmail.com>
2021-03-30 22:27:13 +0700

    test hbmqtt

---
+++ hbmqttClient.py
@@ -0,0 +1,39 @@
+import asyncio
+from queueHandler import Message
+
+from hbmqtt.client import MQTTClient, ConnectException
+from hbmqtt.mqtt.constants import QOS_1, QOS_2
+
+#
+# This sample shows how to publish messages to broker using different QOS
+# Debug outputs shows the message flows
+#
+@asyncio.coroutine
+def test_coro():
+    C = MQTTClient()
+    yield from C.connect('mqtt://113.161.79.146:5000/')
+    tasks = [
+        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_0')),
+        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_1', qos=QOS_1)),
+        asyncio.ensure_future(C.publish('a/b', b'TEST MESSAGE WITH QOS_2', qos=QOS_2)),
+    ]
+    yield from asyncio.wait(tasks)
+    yield from C.disconnect()
+
+
+@asyncio.coroutine
+def test_coro2():
+    try:
+        C = MQTTClient()
+        yield from C.connect('mqtt://113.161.79.146:5000/')
+        yield from C.publish('$SYS/iot2050No1/temp', b'30', qos=0x00)
+        yield from C.publish('$SYS/iot2050No1/humidity', b'80', qos=0x01)
+        yield from C.publish('$SYS/iot2050No1/pressure', b'5', qos=0x02)
+        yield from C.disconnect()
+    except ConnectException as ce:
+        asyncio.get_event_loop().stop()
+
+
+if __name__ == '__main__':
+#    asyncio.get_event_loop().run_until_complete(test_coro())
+    asyncio.get_event_loop().run_until_complete(test_coro2())
---
+++ queueHandler.py
@@ -0,0 +1,39 @@
+import asyncio
+import aioredis
+import json
+from collections import OrderedDict
+
+class Message(object):
+    def __init__(self, redisHost, redisPort):
+        self.redisHost = redisHost
+        self.redisPort = redisPort
+        self.workers = dict()
+        self.redis = None
+        self.channels = []
+
+    def add_worker(self, workName, workerInstance):
+        self.workers[workName] = workerInstance
+
+    async def connect_to_redis(self):
+        conn = 'redis://' + self.redisHost + ':' + str(self.redisPort)
+        print(conn)
+        self.redis = await aioredis.create_redis(conn)
+
+    async def send_message(self, channel, message):
+        print('About to publish %s to channel %s', message, channel)
+        await self.redis.publish(channel, message)
+
+    async def add_channel(self, *channels):
+        if len(channels)>0:
+            for channel in channels:
+                res = await self.redis.subscribe(channel)
+                self.channels.append(res[0])
+                tsk = asyncio.ensure_future(self.callback_message_comes(res[0]))
+                await tsk
+    
+    async def callback_message_comes(self, channel):
+        while (await channel.wait_message()):
+            msg = await channel.get_json()
+            print("Got Message:", msg)
+            worker = self.workers.get(msg['type'], "nothing")
+            worker.perform(msg)
