64e4b2deac1db90dda1fb529d2167e3f7cb25767
tienvinhle <tienvinh.industrial.automation@gmail.com>
2021-04-08 01:09:08 +0700

    edit

--- mqttClient.py
+++ mqttClient.py
@@ -79,9 +79,10 @@ class MQTTClient:
 
     def publish_msg(self, topic, message, QoS):
         encoded_msg = str(message).encode()
+        print()
         self.client.publish(topic, encoded_msg, QoS)
 
     def perform(self, obj):
         for (datapoint, msg) in obj.items():
-            topic = self.orgID + '/' + datapoint.replace('data','')
+            topic = self.orgID + datapoint.replace('data','')
             self.publish_msg(topic, msg, self.qos)
